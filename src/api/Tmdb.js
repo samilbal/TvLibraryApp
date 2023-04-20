520bd1275f50eafa192507ae61a330494ee7e1fc
Şamil Bal <mehmetsamilbal417@gmail.com>
2023-04-20 16:28:42 +0300

    Delete src/api directory

--- src/api/Tmdb.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGVmZTg0ZWRlOTBlOTIyZjY5YjYzNTBkYzhiNTEzOCIsInN1YiI6IjY0MjgwNzc2OTYwY2RlMDEyNjM0NjgxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kgOkEYLqojWisAIMdMqpv3gbrxhZKd37-SBSDk2G8t4",
-  },
-});
